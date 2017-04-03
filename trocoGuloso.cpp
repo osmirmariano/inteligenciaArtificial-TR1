01365d9c1ff5c26892499dcd4685fd4050ea803c
Osmir Mariano <osmirmariano@uft.edu.br>
2017-04-03 16:32:11 -0300

    Versão final da implementação

--- trocoGuloso.cpp
+++ trocoGuloso.cpp
@@ -2,12 +2,34 @@
 #include <vector>
 using namespace std;
 
+/*
+	Caso de teste passo a passo:
+	troco = 26
+	M = {1, 2, 5, 10, 20}
+
+	Começando pela a maior moeda
+	auxMoedas = 26/20 = 1
+	troco = 26-1*20 = 6
+	quantMoedas = 1
+
+	auxMoedas =6/10 = 
+	troco = 6-0*20 = 6
+	quantMoedas = 1 + 0 = 1
+
+	auxMoedas = 6/5 = 1
+	troco = 6-1*5 = 1
+	quantMoedas = 1+1 = 2
+
+	auxMoedas = 1/1 = 1
+	troco = 1-1*1 = 0
+	quantMoedas = 2+1 = 3
+*/
 int numMoedas(vector<int>& moedas, int troco) {
 	int quantMoedas = 0, auxMoedas, x;
-	
+	//Correto
 	for(x = moedas.size()-1; x >= 0; x--){
-		int auxMoedas = troco/moedas[x];
-		troco -= auxMoedas*moedas[x];
+		int auxMoedas = troco/moedas[x]; 
+		troco -= auxMoedas*moedas[x]; 
 		quantMoedas += auxMoedas;
 	}
 	return quantMoedas;
